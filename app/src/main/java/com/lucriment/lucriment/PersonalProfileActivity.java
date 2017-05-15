47838f01b36b41cba2925fcb5f6a34316df3365f
Chris Behan <chris@lucriment.com>
2017-05-15 13:57:56 -0600

    basic photo upload

--- app/src/main/java/com/lucriment/lucriment/PersonalProfileActivity.java
+++ app/src/main/java/com/lucriment/lucriment/PersonalProfileActivity.java
@@ -1,13 +1,20 @@
 package com.lucriment.lucriment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +22,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class PersonalProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,6 +41,11 @@ public class PersonalProfileActivity extends AppCompatActivity implements View.O
     private UserInfo userInfo;
     private EditText editBioText;
     private FirebaseUser user;
+    private Button uploadButton;
+    private StorageReference storageReference;
+    private ProgressDialog picUploadDialog;
+    private static final int GALLERYINTENT = 2;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +53,7 @@ public class PersonalProfileActivity extends AppCompatActivity implements View.O
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
         final String currentKey = firebaseAuth.getCurrentUser().getUid();
         if(userInfo == null) {
             if (getIntent().hasExtra("userInfo")) {
@@ -68,7 +85,9 @@ public class PersonalProfileActivity extends AppCompatActivity implements View.O
         bioField = (TextView) findViewById(R.id.tutorBioField);
         editBioText = (EditText) findViewById(R.id.editBioField);
         editButton = (Button) findViewById(R.id.editButton);
-
+        uploadButton = (Button) findViewById(R.id.uploadPhoto);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        picUploadDialog = new ProgressDialog(this);
 
 
         personalName.setText(firebaseAuth.getCurrentUser().getDisplayName());
@@ -80,12 +99,40 @@ public class PersonalProfileActivity extends AppCompatActivity implements View.O
 
         editButton.setOnClickListener(this);
         backButton.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
 
         //setup buttons and fields
         //  tutorName.setText();
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERYINTENT && resultCode == RESULT_OK){
+            picUploadDialog.setMessage("Uploading...");
+            picUploadDialog.show();
+
+            Uri uri = data.getData();
+            StorageReference profilePicPath = storageReference.child("ProfilePics").child(firebaseAuth.getCurrentUser().getUid());
+            profilePicPath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    picUploadDialog.dismiss();
+                    Toast.makeText(PersonalProfileActivity.this, "Upload Complete", Toast.LENGTH_SHORT).show();
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    Picasso.with(PersonalProfileActivity.this).load(downloadUri).fit().centerCrop().into(imageView);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        }
+    }
 
     @Override
     public void onClick(View v) {
@@ -114,6 +161,11 @@ public class PersonalProfileActivity extends AppCompatActivity implements View.O
                 databaseReference.child("Students").child(user.getUid()).setValue(userInfo);
             }
 
+        }
+        if(v == uploadButton){
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, GALLERYINTENT);
         }
 
     }
