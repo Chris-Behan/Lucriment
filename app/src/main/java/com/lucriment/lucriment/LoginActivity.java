8fd56c514b52e69a0eab45c7081903b0e2840918
Chris Behan <chris@lucriment.com>
2017-05-29 18:15:11 -0600

    V0.9

--- app/src/main/java/com/lucriment/lucriment/LoginActivity.java
+++ app/src/main/java/com/lucriment/lucriment/LoginActivity.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
